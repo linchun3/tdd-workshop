5305a64f9fcfb0b5721fb296bec4d22079a30372
Lee Hao Ran <haoranlee@gmail.com>
2026-01-16 12:23:17 +0800

    PDO-XXX Step 5 uncompleted

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,13 +2,16 @@ def add(numbers: str) -> int:
     if not numbers:
         return 0
 
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[4:]
+        numbers = numbers.replace(delimiter, ",")
     numbers = numbers.replace("\n", ",")
     if numbers[-1] == ",":
         raise ValueError("separators should not appear at the conclusion")
     if "," not in numbers:
         return int(numbers)
     nums = numbers.split(",")
-    print(nums)
     total = 0
     for num in nums:
         total += int(num)
