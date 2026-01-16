b67b7efb952af53bf25aebbbf01cdc4db04bd0ce
ACMH1 <alex.chua.ming.hao@gmail.com>
2026-01-16 12:00:00 +0800

    Split test cases

--- string_calculator/test_calculator.py
+++ string_calculator/test_calculator.py
@@ -1,16 +1,27 @@
 from string_calculator.calculator import add
 import pytest
 
-def test_sum():
+def test_empty_str():
     assert add("") == 0
+
+def test_single_num():
     assert add("1") == 1
+
+def test_double_num():
     assert add("1,2") == 3
+
+def test_triple_num():
     assert add("1,2,3") == 6
+
+def test_line_break():
     assert add("1,2\n3") == 6
     assert add("2,\n3") == 5
+
+def test_special_delimiter():
     assert add("//;\n1;3") == 4
     assert add("//|\n1|2|3") == 6
 
+def test_ends_in_delimiter():
     with pytest.raises(ValueError):
         add("4,")
 
