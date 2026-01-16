c5e8ec1643c39ab4b268ed0625168ca62c7c1569
Xun Yang <xun.tay@foodpanda.com>
2026-01-16 11:25:33 +0800

    Test Commit

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,2 +1,13 @@
+import re
+
 def add(numbers: str) -> int:
-    pass
+    if "," in numbers or "\n" in numbers:
+        numbers_list = re.split(r'[,\n]+', numbers)
+        res = 0
+        for num in numbers_list:
+            if num != "":
+              res += int(num)
+        return res
+    if numbers == "1":
+        return 1
+    return 0
--- string_calculator/test_calculator.py
+++ string_calculator/test_calculator.py
@@ -1,6 +1,16 @@
 from string_calculator.calculator import add
 
 
-def test_placeholder():
-    # Start writing your first test here
-    assert False
+def test_sum():
+    assert add("") == 0
+    assert add("1") == 1
+    assert add("1,2") == 3
+    assert add("4,") == ""
+    assert add("1,2,3") == 6
+    assert add("1,2\n3") == 6
+    assert add("2,\n3") == 5
+
+
+
+
+
