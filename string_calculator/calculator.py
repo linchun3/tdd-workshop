3e7a82edc31e992d79032e6154c80ecfe51473a5
Lee Hao Ran <haoranlee@gmail.com>
2026-01-16 11:40:00 +0800

    PDO-XXX Step 3

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,4 +1,6 @@
 def add(numbers: str) -> int:
+    numbers = numbers.replace("\n", ",")
+    print(numbers)
     if not numbers:
         return 0
     if "," not in numbers:
--- string_calculator/test_calculator.py
+++ string_calculator/test_calculator.py
@@ -1,7 +1,9 @@
+import pytest
+
 from string_calculator.calculator import add
 
 
-def test_placeholder():
+def test_step_1():
     # Start writing your first test here
     assert add("") == 0
     assert add("1") == 1
@@ -9,8 +11,16 @@ def test_placeholder():
     assert add("1,2") == 3
 
 
-def test_more_than_2():
+def test_step_2():
     assert add("1,2,3") == 6
+    assert add("1,2,3,4,5") == 15
 
-def test_flexible_separators():
-    
+
+def test_step_3():
+    assert add("1,2\n3") == 6
+
+
+def test_step_4():
+    with pytest.raises(ValueError) as excinfo:
+        add("1,2,")
+    assert excinfo.value.message == "separators should not appear at the conclusion"
