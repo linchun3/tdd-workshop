0bed34320c59ebd18bbf44b40c974f4db7b09baf
ACMH1 <alex.chua.ming.hao@gmail.com>
2026-01-16 11:51:14 +0800

    Add handling for case 5

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,6 +1,11 @@
 import re
 
 def add(numbers: str) -> int:
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[3:].replace(delimiter, ",")
+    if len(numbers) > 0 and numbers[-1] == ",":
+        raise ValueError
     if "," in numbers or "\n" in numbers:
         numbers_list = re.split(r'[,\n]+', numbers)
         res = 0
