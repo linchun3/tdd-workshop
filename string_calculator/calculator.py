4c66231dfa2b9e0565de2b37e9a3b3f206921fe5
Zachary Tan <60475836+zacharyt-cs@users.noreply.github.com>
2026-01-16 11:29:51 +0800

    first round

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,2 +1,10 @@
 def add(numbers: str) -> int:
-    pass
+    if not numbers:
+        return 0
+    if "," not in numbers:
+        return int(numbers)
+    nums = numbers.split(",")
+    total = 0
+    for num in nums:
+        total += int(num)
+    return total
--- string_calculator/test_calculator.py
+++ string_calculator/test_calculator.py
@@ -3,4 +3,14 @@ from string_calculator.calculator import add
 
 def test_placeholder():
     # Start writing your first test here
-    assert False
+    assert add("") == 0
+    assert add("1") == 1
+    assert add("12") == 12
+    assert add("1,2") == 3
+
+
+def test_more_than_2():
+    assert add("1,2,3") == 6
+
+def test_flexible_separators():
+    
