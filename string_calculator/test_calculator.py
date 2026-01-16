58330742f9cb2aa63834f8a49ff7c50487ab8d8d
Joy Sng <joy.sng@foodpanda.com>
2026-01-16 11:39:27 +0800

    raise valueerror for step 4

--- string_calculator/test_calculator.py
+++ string_calculator/test_calculator.py
@@ -1,17 +1,15 @@
 from string_calculator.calculator import add
-
+import pytest
 
 def test_sum():
     assert add("") == 0
     assert add("1") == 1
     assert add("1,2") == 3
-    assert add("4,") == ""
     assert add("1,2,3") == 6
     assert add("1,2\n3") == 6
     assert add("2,\n3") == 5
     assert add("//;\n1;3") == 4
     assert add("//|\n1|2|3") == 6
 
-
-
-
+    with pytest.raises(ValueError):
+        add("4,")
